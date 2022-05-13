d48ae78112aa9dccff7e9ac25cfeb2e99f332944
Razia <nrwpc.stack@gmail.com>
2022-05-14 00:47:50 +0800

    remove unused import

--- src/foliaging/views/Checkout.jsx
+++ src/foliaging/views/Checkout.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import { ContentFeature } from "../components";
 import fluidImg from "../../assets/images/fluid_monstera.svg";
 
