2c7c1d265066f9b5962c44969354fe782bccaf24
Razia <nrwpc.stack@gmail.com>
2022-05-13 11:59:20 +0800

    removed unused imports

--- src/foliaging/views/Products.jsx
+++ src/foliaging/views/Products.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import fluidImage from "../../assets/images/fluid_palm.svg";
-import { alpha, Box, Card, CardMedia, Typography } from "@mui/material";
 import { HeaderBanner, ProductsListing } from "../components";
-import { ContentBox, FlexBox, FrostedContentBox } from "../styles/components";
-import siteColors from "../styles/colors";
+import { ContentBox } from "../styles/components";
 
 export default function Products() {
   return (
