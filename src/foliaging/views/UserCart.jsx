6443496f3304ea427a775f760f347ddeb8c47c17
Razia <nrwpc.stack@gmail.com>
2022-05-13 12:54:01 +0800

    add message when there is no item in cart

--- src/foliaging/views/UserCart.jsx
+++ src/foliaging/views/UserCart.jsx
@@ -48,51 +48,58 @@ export default function UserCart() {
         <LeafLoader />
       ) : (
         <FlexBox sx={{ my: 4, flexDirection: "column", gap: 2 }}>
-          {state.cart?.map((item) => (
-            <FrostedContentBox key={item.id}>
-              <FlexBox sx={{ p: 2 }}>
-                <Card sx={{ maxWidth: "20%" }}>
-                  <CardMedia component="img" image={item.images[0]} />
-                </Card>
-                <FlexBox
-                  sx={{ ml: 4, flexDirection: { xs: "column", md: "row" } }}>
+          {state.cart.length ? (
+            state.cart.map((item) => (
+              <FrostedContentBox key={item.id}>
+                <FlexBox sx={{ p: 2 }}>
+                  <Card sx={{ maxWidth: "20%" }}>
+                    <CardMedia component="img" image={item.images[0]} />
+                  </Card>
                   <FlexBox
-                    sx={{ flexDirection: "column", alignItems: "flex-start" }}>
-                    <Typography>{item.product.title}</Typography>
-                    <Typography>${item.product.price.toFixed(2)}</Typography>
-                  </FlexBox>
-                  <FlexBox
-                    sx={{
-                      mt: { xs: 2, md: 0 },
-                      justifyContent: { xs: "flex-start", md: "flex-end" },
-                      maxWidth: { xs: "100%", md: "35%" },
-                    }}>
-                    <TextField
-                      type="number"
-                      size="small"
-                      label="Quantity"
-                      name="quantity"
-                      value={item.quantity}
-                      onChange={(evt) =>
-                        handleChange(
-                          item.customer_id,
-                          item.product_id,
-                          evt.target.value
-                        )
-                      }
-                    />
-                    <IconButton
-                      color="secondary"
-                      onClick={(evt) =>
-                        handleRemove(item.customer_id, item.product_id)
-                      }>
-                      <Icon className="ri-delete-bin-2-line" />
-                    </IconButton>
+                    sx={{ ml: 4, flexDirection: { xs: "column", md: "row" } }}>
+                    <FlexBox
+                      sx={{
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                      }}>
+                      <Typography>{item.product.title}</Typography>
+                      <Typography>${item.product.price.toFixed(2)}</Typography>
+                    </FlexBox>
+                    <FlexBox
+                      sx={{
+                        mt: { xs: 2, md: 0 },
+                        justifyContent: { xs: "flex-start", md: "flex-end" },
+                        maxWidth: { xs: "100%", md: "35%" },
+                      }}>
+                      <TextField
+                        type="number"
+                        size="small"
+                        label="Quantity"
+                        name="quantity"
+                        value={item.quantity}
+                        onChange={(evt) =>
+                          handleChange(
+                            item.customer_id,
+                            item.product_id,
+                            evt.target.value
+                          )
+                        }
+                      />
+                      <IconButton
+                        color="secondary"
+                        onClick={(evt) =>
+                          handleRemove(item.customer_id, item.product_id)
+                        }>
+                        <Icon className="ri-delete-bin-2-line" />
+                      </IconButton>
+                    </FlexBox>
                   </FlexBox>
                 </FlexBox>
-              </FlexBox>
-            </FrostedContentBox>
-          ))}
+              </FrostedContentBox>
+            ))
+          ) : (
+            <Typography variant="h6">No items in cart</Typography>
+          )}
         </FlexBox>
       )}
     </Fragment>
