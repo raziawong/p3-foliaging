1b93249d777c549b846756184e7c474dc2aa8c10
Razia <nrwpc.stack@gmail.com>
2022-05-14 01:28:00 +0800

    remove unused import

--- src/foliaging/views/NotFound.jsx
+++ src/foliaging/views/NotFound.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import { ContentFeature } from "../components";
 import fluidImg from "../../assets/images/fluid_monstera.svg";
 
 export default function NotFound() {
-  const params = useParams();
-
   return (
     <ContentFeature
       header="Not Found"
