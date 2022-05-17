545fce07360547415f9c62ce9e55d4e02dfc8062
Razia <nrwpc.stack@gmail.com>
2022-05-17 11:09:10 +0800

    update base api url

--- src/foliaging/utils/api.js
+++ src/foliaging/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { sortOptions } from "./helpers";
 
 export const apiBase = axios.create({
-  baseURL: "https://tgc16-foliaging-express.herokuapp.com/",
+  baseURL: "https://tgc16-foliaging-express.herokuapp.com/api",
 });
 
 export const apiPaths = {
